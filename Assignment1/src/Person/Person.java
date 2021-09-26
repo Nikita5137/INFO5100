1b07cfaae778c36676051c0a1cbf02a5e5572419
Nikita Singh <singh.niki@northeastern.edu>
2021-09-26 12:42:17 -0400

    Initial commit

---
+++ Assignment1/src/Person/Person.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Person;
+
+import java.awt.Image;
+import java.util.Date;
+
+/**
+ *
+ * @author nikita
+ */
+public class Person {
+    private String name;
+    private String geoData;
+private String db;
+private String telepNumber;
+private String faxNumber;
+private String emailid;
+private String ssn;
+private String medRecNumber;
+private String healthPlanNo;
+private String bankAccNumber;
+private String licensePlate;
+private String vehicleSerialNo;
+private String DeviceNo;
+private String linkedIn;
+private String internetProtocolAdd;
+private String biomerticIdentifier;
+private String Image;
+private String idCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeoData() {
+        return geoData;
+    }
+
+    public void setGeoData(String geoData) {
+        this.geoData = geoData;
+    }
+
+    public String getDb() {
+        return db;
+    }
+
+    public void setDb(String db) {
+        this.db = db;
+    }
+
+    public String getTelepNumber() {
+        return telepNumber;
+    }
+
+    public void setTelepNumber(String telepNumber) {
+        this.telepNumber = telepNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedRecNumber() {
+        return medRecNumber;
+    }
+
+    public void setMedRecNumber(String medRecNumber) {
+        this.medRecNumber = medRecNumber;
+    }
+
+    public String getHealthPlanNo() {
+        return healthPlanNo;
+    }
+
+    public void setHealthPlanNo(String healthPlanNo) {
+        this.healthPlanNo = healthPlanNo;
+    }
+
+    public String getBankAccNumber() {
+        return bankAccNumber;
+    }
+
+    public void setBankAccNumber(String bankAccNumber) {
+        this.bankAccNumber = bankAccNumber;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getVehicleSerialNo() {
+        return vehicleSerialNo;
+    }
+
+    public void setVehicleSerialNo(String vehicleSerialNo) {
+        this.vehicleSerialNo = vehicleSerialNo;
+    }
+
+    public String getDeviceNo() {
+        return DeviceNo;
+    }
+
+    public void setDeviceNo(String DeviceNo) {
+        this.DeviceNo = DeviceNo;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getInternetProtocolAdd() {
+        return internetProtocolAdd;
+    }
+
+    public void setInternetProtocolAdd(String internetProtocolAdd) {
+        this.internetProtocolAdd = internetProtocolAdd;
+    }
+
+    public String getBiomerticIdentifier() {
+        return biomerticIdentifier;
+    }
+
+    public void setBiomerticIdentifier(String biomerticIdentifier) {
+        this.biomerticIdentifier = biomerticIdentifier;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String Image) {
+        this.Image = Image;
+    }
+
+    public String getIdCode() {
+        return idCode;
+    }
+
+    public void setIdCode(String idCode) {
+        this.idCode = idCode;
+    }
+
+    
+    }
